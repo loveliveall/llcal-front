1fcb5147e7215426d5c4385bbbaae160dee97b7d
LoveLive <lovelivefam@gmail.com>
2020-07-05 22:11:36 +0900

    Modify default theme

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import App from '@/components/App';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffa400',
+    },
+    secondary: {
+      main: '#ff7f32',
+    },
+  },
+});
+
 ReactDOM.render(
   <>
     <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </>,
   document.getElementById('app'),
 );
