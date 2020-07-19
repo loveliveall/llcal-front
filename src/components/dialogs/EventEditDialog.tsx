d194431b36d2d480b5e24b1981ce596b4ed74722
LoveLive <lovelivefam@gmail.com>
2020-07-19 15:03:56 +0900

    Some preformance optimization on event edit dialog

--- src/components/dialogs/EventEditDialog.tsx
+++ src/components/dialogs/EventEditDialog.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import RRule from 'rrule';
 import { useSelector, useDispatch } from 'react-redux';
 import addMinutes from 'date-fns/addMinutes';
 
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
 import Switch from '@material-ui/core/Switch';
 import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
-
-import {
-  DatePicker,
-  DateTimePicker,
-} from '@material-ui/pickers';
-import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 
 import CloseIcon from '@material-ui/icons/Close';
 import SaveIcon from '@material-ui/icons/Save';
@@ -37,14 +25,20 @@ import useMobileCheck from '@/hooks/useMobileCheck';
 import {
   addNewEvent,
 } from '@/api';
-import { voiceActorList, categoryGroupList, eventCategoryList } from '@/commonData';
+import { voiceActorList } from '@/commonData';
 import { AppState } from '@/store';
 import { clearToken } from '@/store/auth/actions';
 import { closeEventEditDialog } from '@/store/edit-dialog/actions';
 import { refreshHash } from '@/store/flags/actions';
-import { rruleToText } from '@/utils';
 
-import RRuleEditModal from './RRuleEditModal';
+import {
+  EditRangeEditor, EditRangeEditorProps,
+  TitleEditor,
+  PlaceEditor,
+  DateInfoEditor,
+  DescriptionEditor,
+  CategoryEditor,
+} from './EventEditDialogComp';
 
 const useStyles = makeStyles((theme: Theme) => ({
   dialogTitle: {
@@ -70,8 +64,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-const jsDayToWeekday = [RRule.SU, RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR, RRule.SA];
-
 const EventEditDialog: React.FC = () => {
   const classes = useStyles();
   const isMobile = useMobileCheck();
@@ -83,24 +75,24 @@ const EventEditDialog: React.FC = () => {
   const origEvent = useSelector((state: AppState) => state.editDialog.event);
   const [loading, setLoading] = React.useState(false);
   const [errMsg, setErrMsg] = React.useState('');
-  const [editRange, setEditRange] = React.useState<'this' | 'after' | 'all'>('this');
+  const [editRange, setEditRange] = React.useState<EditRangeEditorProps['editRange']>(null);
   const [title, setTitle] = React.useState('');
   const [place, setPlace] = React.useState('');
   const [allDay, setAllDay] = React.useState(false);
   const [start, setStart] = React.useState(initialStart);
   const [end, setEnd] = React.useState(addMinutes(initialStart, 30));
   const [rrule, setRRule] = React.useState('');
-  const [rruleModalOpen, setRRuleModalOpen] = React.useState(false);
   const [desc, setDesc] = React.useState('');
-  const [categoryId, setCategroyId] = React.useState<number | null>(null);
+  const [categoryId, setCategoryId] = React.useState<number | null>(null);
   const [isLoveLive, setIsLoveLive] = React.useState(false);
   const [isRepeating, setIsRepeating] = React.useState(false);
   const [vaIdList, setVAIdList] = React.useState<number[]>([]);
+  const isEditRepeat = origEvent !== null && origEvent.rrule !== '';
 
   React.useEffect(() => {
     const nowStart = new Date();
     nowStart.setMinutes(Math.floor(nowStart.getMinutes() / 15) * 15);
-    setEditRange('this');
+    setEditRange(null);
     setTitle(origEvent === null ? '' : origEvent.title);
     setPlace(origEvent === null ? '' : origEvent.place);
     setAllDay(origEvent === null ? false : origEvent.allDay);
@@ -108,7 +100,7 @@ const EventEditDialog: React.FC = () => {
     setEnd(origEvent === null ? addMinutes(nowStart, 30) : origEvent.endTime);
     setRRule(origEvent === null ? '' : origEvent.rrule);
     setDesc(origEvent === null ? '' : origEvent.description);
-    setCategroyId(origEvent === null ? null : origEvent.categoryId);
+    setCategoryId(origEvent === null ? null : origEvent.categoryId);
     setIsLoveLive(origEvent === null ? false : origEvent.isLoveLive);
     setIsRepeating(origEvent === null ? false : origEvent.isRepeating);
     setVAIdList(origEvent === null ? [] : origEvent.voiceActorIds);
@@ -120,7 +112,9 @@ const EventEditDialog: React.FC = () => {
     dispatch(closeEventEditDialog());
   };
   const onSaveClick = () => {
-    if (title === '') {
+    if (isEditRepeat && editRange === null) {
+      setErrMsg('적절한 수정 범위를 반드시 선택해야 합니다');
+    } else if (title === '') {
       setErrMsg('제목이 반드시 필요합니다');
     } else if (start > end) {
       setErrMsg('일정의 끝은 반드시 시작 이후여야 합니다');
@@ -155,38 +149,6 @@ const EventEditDialog: React.FC = () => {
       });
     }
   };
-  const handleStartChange = (date: MaterialUiPickersDate) => {
-    if (date !== null) {
-      setStart(date);
-      if (end < date) {
-        setEnd(addMinutes(date, 30));
-      }
-      // Update rrule for weekly repeat
-      const opt = RRule.parseString(rrule);
-      if (opt.freq === RRule.WEEKLY) {
-        const match = /BYDAY=(.*?)(;|$)/m.exec(rrule);
-        if (match) {
-          const weekdayList = match[1].split(',');
-          if (weekdayList.length === 1) {
-            opt.byweekday = [jsDayToWeekday[date.getDay()]];
-            setRRule(RRule.optionsToString(opt));
-          }
-        }
-      }
-    }
-  };
-  const handleEndChange = (date: MaterialUiPickersDate) => {
-    if (date !== null) {
-      if (date < start) {
-        setEnd(addMinutes(start, 30));
-      } else {
-        setEnd(date);
-      }
-    }
-  };
-  const openRRuleModal = () => {
-    setRRuleModalOpen(true);
-  };
   return (
     <Dialog
       open={open}
@@ -217,169 +179,45 @@ const EventEditDialog: React.FC = () => {
           <Typography className={classes.error}>{errMsg}</Typography>
         )}
         {/* Edit range */}
-        {origEvent !== null && origEvent.rrule !== '' && (
-          <GridContainer>
-            <Grid item xs={2}>
-              <Typography>수정 범위</Typography>
-            </Grid>
-            <Grid item xs>
-              <Select
-                value={editRange}
-                onChange={(e) => setEditRange(e.target.value as 'this' | 'after' | 'all')}
-              >
-                <MenuItem value="this">이 일정만</MenuItem>
-                <MenuItem value="after">이 일정 및 향후 일정</MenuItem>
-                <MenuItem value="all">모든 반복 일정</MenuItem>
-              </Select>
-            </Grid>
-          </GridContainer>
+        {isEditRepeat && (
+          <EditRangeEditor
+            editRange={editRange}
+            setEditRange={setEditRange}
+          />
         )}
         {/* Title */}
-        <GridContainer>
-          <Grid item xs={2}>
-            <Typography>제목</Typography>
-          </Grid>
-          <Grid item xs>
-            <Input
-              id="title"
-              value={title}
-              error={title === ''}
-              onChange={(e) => setTitle(e.target.value)}
-              fullWidth
-              inputProps={{
-                autoCapitalize: 'none',
-                autoCorrect: 'off',
-              }}
-            />
-          </Grid>
-        </GridContainer>
+        <TitleEditor
+          title={title}
+          setTitle={setTitle}
+        />
         {/* Place */}
-        <GridContainer>
-          <Grid item xs={2}>
-            <Typography>장소</Typography>
-          </Grid>
-          <Grid item xs>
-            <Input
-              id="place"
-              value={place}
-              onChange={(e) => setPlace(e.target.value)}
-              fullWidth
-              inputProps={{
-                autoCapitalize: 'none',
-                autoCorrect: 'off',
-              }}
-            />
-          </Grid>
-        </GridContainer>
+        <PlaceEditor
+          place={place}
+          setPlace={setPlace}
+        />
         {/* Date */}
-        <GridContainer>
-          <Grid item xs={2} />
-          <Grid item xs>
-            <Typography component="span">하루 종일</Typography>
-            <Switch
-              checked={allDay}
-              onChange={() => setAllDay((prev) => !prev)}
-            />
-          </Grid>
-        </GridContainer>
-        <GridContainer>
-          <Grid item xs={2}>
-            <Typography>시작</Typography>
-          </Grid>
-          <Grid item xs>
-            {allDay ? (
-              <DatePicker
-                value={start}
-                onChange={handleStartChange}
-                format="yyyy/MM/dd"
-              />
-            ) : (
-              <DateTimePicker
-                value={start}
-                onChange={handleStartChange}
-                format="yyyy/MM/dd HH:mm"
-                minutesStep={5}
-              />
-            )}
-          </Grid>
-        </GridContainer>
-        <GridContainer>
-          <Grid item xs={2}>
-            <Typography>종료</Typography>
-          </Grid>
-          <Grid item xs>
-            {allDay ? (
-              <DatePicker
-                value={end}
-                minDate={start}
-                onChange={handleEndChange}
-                format="yyyy/MM/dd"
-              />
-            ) : (
-              <DateTimePicker
-                value={end}
-                minDate={start}
-                onChange={handleEndChange}
-                format="yyyy/MM/dd HH:mm"
-                minutesStep={5}
-              />
-            )}
-          </Grid>
-        </GridContainer>
-        <GridContainer>
-          <Grid item xs={2}>
-            <Typography>반복</Typography>
-          </Grid>
-          <Grid item xs>
-            <Button variant="outlined" onClick={openRRuleModal}>
-              {rrule === '' ? '반복 없음' : rruleToText(start, rrule)}
-            </Button>
-          </Grid>
-        </GridContainer>
+        <DateInfoEditor
+          allDay={allDay}
+          setAllDay={setAllDay}
+          start={start}
+          setStart={setStart}
+          end={end}
+          setEnd={setEnd}
+          rrule={rrule}
+          setRRule={setRRule}
+          setIsRepeating={setIsRepeating}
+          isFreqEditDisabled={isEditRepeat}
+        />
         {/* Description */}
-        <GridContainer>
-          <Grid item xs={2}>
-            <Typography>상세 설명</Typography>
-          </Grid>
-          <Grid item xs>
-            <Input
-              id="description"
-              value={desc}
-              onChange={(e) => setDesc(e.target.value)}
-              fullWidth
-              multiline
-              inputProps={{
-                autoCapitalize: 'none',
-                autoCorrect: 'off',
-              }}
-            />
-          </Grid>
-        </GridContainer>
+        <DescriptionEditor
+          description={desc}
+          setDescription={setDesc}
+        />
         {/* Category */}
-        <GridContainer>
-          <Grid item xs={2}>
-            <Typography>분류</Typography>
-          </Grid>
-          <Grid item xs>
-            <Select
-              value={categoryId === null ? 'None' : categoryId}
-              onChange={(e) => setCategroyId(e.target.value as number)}
-            >
-              <MenuItem value="None">선택 안됨</MenuItem>
-              {eventCategoryList.filter((cat) => cat.groupId === null).map((cat) => (
-                <MenuItem key={`category-${cat.id}`} value={cat.id}>{cat.name}</MenuItem>
-              ))}
-              {categoryGroupList.map((group) => (
-                [
-                  <ListSubheader>{group.name}</ListSubheader>,
-                  eventCategoryList.filter((cat) => cat.groupId === group.id).map((cat) => (
-                    <MenuItem key={`category-${cat.id}`} value={cat.id}>{cat.name}</MenuItem>
-                  )),
-                ]
-              ))}
-            </Select>
-          </Grid>
-        </GridContainer>
+        <CategoryEditor
+          categoryId={categoryId}
+          setCategoryId={setCategoryId}
+        />
         {/* Is LoveLive */}
         <GridContainer>
           <Grid item xs={2} />
@@ -420,17 +258,6 @@ const EventEditDialog: React.FC = () => {
           </Grid>
         </GridContainer>
       </DialogContent>
-      <RRuleEditModal
-        open={rruleModalOpen}
-        setOpen={(o) => setRRuleModalOpen(o)}
-        start={origEvent === null ? new Date() : origEvent.startTime}
-        rrule={rrule}
-        setRRule={(r) => {
-          setRRule(r);
-          setIsRepeating(true);
-        }}
-        isFreqEditDisabled={origEvent !== null && origEvent.rrule !== ''}
-      />
       <Backdrop className={classes.backdrop} open={loading}>
         <CircularProgress color="inherit" />
       </Backdrop>
---
+++ src/components/dialogs/EventEditDialogComp.tsx
@@ -0,0 +1,309 @@
+import React from 'react';
+import RRule from 'rrule';
+import addMinutes from 'date-fns/addMinutes';
+
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Input from '@material-ui/core/Input';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Switch from '@material-ui/core/Switch';
+import Typography from '@material-ui/core/Typography';
+
+import { DatePicker, DateTimePicker } from '@material-ui/pickers';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
+
+import GridContainer from '@/components/common/GridContainer';
+
+import { rruleToText } from '@/utils';
+import { eventCategoryList, categoryGroupList } from '@/commonData';
+import RRuleEditModal from './RRuleEditModal';
+
+type EditRange = 'this' | 'after' | 'all';
+
+export type EditRangeEditorProps = {
+  editRange: null | EditRange,
+  setEditRange: (newValue: null | EditRange) => void,
+};
+const EditRangeEditorComp: React.FC<EditRangeEditorProps> = ({
+  editRange, setEditRange,
+}) => (
+  <GridContainer>
+    <Grid item xs={2}>
+      <Typography>수정 범위</Typography>
+    </Grid>
+    <Grid item xs>
+      <Select
+        value={editRange === null ? 'None' : editRange}
+        error={editRange === null}
+        onChange={(e) => setEditRange(e.target.value as null | EditRange)}
+      >
+        <MenuItem value="None">선택해 주세요</MenuItem>
+        <MenuItem value="this">이 일정만</MenuItem>
+        <MenuItem value="after">이 일정 및 향후 일정</MenuItem>
+        <MenuItem value="all">모든 반복 일정</MenuItem>
+      </Select>
+    </Grid>
+  </GridContainer>
+);
+export const EditRangeEditor = EditRangeEditorComp;
+
+export type TitleEditorProps = {
+  title: string,
+  setTitle: (newValue: string) => void,
+};
+const TitleEditorComp: React.FC<TitleEditorProps> = ({
+  title, setTitle,
+}) => (
+  <GridContainer>
+    <Grid item xs={2}>
+      <Typography>제목</Typography>
+    </Grid>
+    <Grid item xs>
+      <Input
+        id="title"
+        value={title}
+        error={title === ''}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+        inputProps={{
+          autoCapitalize: 'none',
+          autoCorrect: 'off',
+        }}
+      />
+    </Grid>
+  </GridContainer>
+);
+export const TitleEditor = TitleEditorComp;
+
+export type PlaceEditorProps = {
+  place: string,
+  setPlace: (newValue: string) => void,
+};
+const PlaceEditorComp: React.FC<PlaceEditorProps> = ({
+  place, setPlace,
+}) => (
+  <GridContainer>
+    <Grid item xs={2}>
+      <Typography>장소</Typography>
+    </Grid>
+    <Grid item xs>
+      <Input
+        id="place"
+        value={place}
+        onChange={(e) => setPlace(e.target.value)}
+        fullWidth
+        inputProps={{
+          autoCapitalize: 'none',
+          autoCorrect: 'off',
+        }}
+      />
+    </Grid>
+  </GridContainer>
+);
+export const PlaceEditor = PlaceEditorComp;
+
+const jsDayToWeekday = [RRule.SU, RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR, RRule.SA];
+export type DateInfoEditorProps = {
+  allDay: boolean,
+  setAllDay: (newValue: boolean) => void,
+  start: Date,
+  setStart: (newValue: Date) => void,
+  end: Date,
+  setEnd: (newValue: Date) => void,
+  rrule: string,
+  setRRule: (newValue: string) => void,
+  setIsRepeating: (newValue: boolean) => void,
+  isFreqEditDisabled: boolean,
+};
+const DateInfoEditorComp: React.FC<DateInfoEditorProps> = ({
+  allDay, setAllDay, start, setStart, end, setEnd, rrule, setRRule, setIsRepeating, isFreqEditDisabled,
+}) => {
+  const [rruleModalOpen, setRRuleModalOpen] = React.useState(false);
+
+  const handleStartChange = (date: MaterialUiPickersDate) => {
+    if (date !== null) {
+      setStart(date);
+      if (end < date) {
+        setEnd(addMinutes(date, 30));
+      }
+      // Update rrule for weekly repeat
+      const opt = RRule.parseString(rrule);
+      if (opt.freq === RRule.WEEKLY) {
+        const match = /BYDAY=(.*?)(;|$)/m.exec(rrule);
+        if (match) {
+          const weekdayList = match[1].split(',');
+          if (weekdayList.length === 1) {
+            opt.byweekday = [jsDayToWeekday[date.getDay()]];
+            setRRule(RRule.optionsToString(opt));
+          }
+        }
+      }
+    }
+  };
+  const handleEndChange = (date: MaterialUiPickersDate) => {
+    if (date !== null) {
+      if (date < start) {
+        setEnd(addMinutes(start, 30));
+      } else {
+        setEnd(date);
+      }
+    }
+  };
+  const openRRuleModal = () => {
+    setRRuleModalOpen(true);
+  };
+
+  return (
+    <>
+      <GridContainer>
+        <Grid item xs={2} />
+        <Grid item xs>
+          <Typography component="span">하루 종일</Typography>
+          <Switch
+            checked={allDay}
+            onChange={() => setAllDay(!allDay)}
+          />
+        </Grid>
+      </GridContainer>
+      <GridContainer>
+        <Grid item xs={2}>
+          <Typography>시작</Typography>
+        </Grid>
+        <Grid item xs>
+          {allDay ? (
+            <DatePicker
+              value={start}
+              onChange={handleStartChange}
+              format="yyyy/MM/dd"
+            />
+          ) : (
+            <DateTimePicker
+              value={start}
+              onChange={handleStartChange}
+              format="yyyy/MM/dd HH:mm"
+              minutesStep={5}
+            />
+          )}
+        </Grid>
+      </GridContainer>
+      <GridContainer>
+        <Grid item xs={2}>
+          <Typography>종료</Typography>
+        </Grid>
+        <Grid item xs>
+          {allDay ? (
+            <DatePicker
+              value={end}
+              minDate={start}
+              onChange={handleEndChange}
+              format="yyyy/MM/dd"
+            />
+          ) : (
+            <DateTimePicker
+              value={end}
+              minDate={start}
+              onChange={handleEndChange}
+              format="yyyy/MM/dd HH:mm"
+              minutesStep={5}
+            />
+          )}
+        </Grid>
+      </GridContainer>
+      <GridContainer>
+        <Grid item xs={2}>
+          <Typography>반복</Typography>
+        </Grid>
+        <Grid item xs>
+          <Button variant="outlined" onClick={openRRuleModal}>
+            {rrule === '' ? '반복 없음' : rruleToText(start, rrule)}
+          </Button>
+        </Grid>
+      </GridContainer>
+      <RRuleEditModal
+        open={rruleModalOpen}
+        setOpen={(o) => setRRuleModalOpen(o)}
+        start={start}
+        rrule={rrule}
+        setRRule={(r) => {
+          setRRule(r);
+          setIsRepeating(true);
+        }}
+        isFreqEditDisabled={isFreqEditDisabled}
+      />
+    </>
+  );
+};
+export const DateInfoEditor = React.memo(
+  DateInfoEditorComp,
+  (prev, next) => (prev.allDay === next.allDay
+      && prev.start === next.start
+      && prev.end === next.end
+      && prev.rrule === next.rrule
+      && prev.isFreqEditDisabled === next.isFreqEditDisabled
+  ),
+);
+
+export type DescriptionEditorProps = {
+  description: string,
+  setDescription: (newValue: string) => void,
+};
+const DescriptionEditorComp: React.FC<DescriptionEditorProps> = ({
+  description, setDescription,
+}) => (
+  <GridContainer>
+    <Grid item xs={2}>
+      <Typography>상세 설명</Typography>
+    </Grid>
+    <Grid item xs>
+      <Input
+        id="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        fullWidth
+        multiline
+        inputProps={{
+          autoCapitalize: 'none',
+          autoCorrect: 'off',
+        }}
+      />
+    </Grid>
+  </GridContainer>
+);
+export const DescriptionEditor = DescriptionEditorComp;
+
+export type CategoryEditorProps = {
+  categoryId: number | null,
+  setCategoryId: (newValue: number | null) => void,
+};
+const CategoryEditorComp: React.FC<CategoryEditorProps> = ({
+  categoryId, setCategoryId,
+}) => (
+  <GridContainer>
+    <Grid item xs={2}>
+      <Typography>분류</Typography>
+    </Grid>
+    <Grid item xs>
+      <Select
+        value={categoryId === null ? 'None' : categoryId}
+        error={categoryId === null}
+        onChange={(e) => setCategoryId(e.target.value as number | null)}
+      >
+        <MenuItem value="None">선택해주세요</MenuItem>
+        {eventCategoryList.filter((cat) => cat.groupId === null).map((cat) => (
+          <MenuItem key={`category-${cat.id}`} value={cat.id}>{cat.name}</MenuItem>
+        ))}
+        {categoryGroupList.map((group) => (
+          [
+            <ListSubheader>{group.name}</ListSubheader>,
+            eventCategoryList.filter((cat) => cat.groupId === group.id).map((cat) => (
+              <MenuItem key={`category-${cat.id}`} value={cat.id}>{cat.name}</MenuItem>
+            )),
+          ]
+        ))}
+      </Select>
+    </Grid>
+  </GridContainer>
+);
+export const CategoryEditor = CategoryEditorComp;
